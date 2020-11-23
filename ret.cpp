6aa722558089b7c1ce4e801e97c5484d99dd20cb
PlutoLove <1580916438@qq.com>
2020-11-23 15:40:40 +0800

    洛谷(待完成)

---
+++ ret.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using namespace std;
+
+#define rep(x,s,t) for(iot i = s; i<=t; i++)
+#define MAXN 10000+5
+
+struct blanket{
+	int a,b,g,k;
+}
+
+blanket rec[MAXN]
+
+int main(){
+	scanf("%d",&n);
+	rep(i,1,n){
+		scanf("%d%d%d%d",&rec[i].a,&rec[i].b,&rec[i].g,&rec[i].k);
+
+	}
+}
